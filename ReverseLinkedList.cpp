b699e1d60cbd2ad423ddbe266d1793493663a6bf
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-10-10 00:22:12 +0530

    Add files via upload

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution{
+public:
+    ListNode *reverselist(ListNode *head){
+        ListNode *curr= head;
+        ListNode *prev= NULL;
+
+        while(curr!=NULL){
+            ListNode *n= curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = n;
+        }
+        return prev;
+        
+    }
+};
+
